e0a321bb72d9bb8849045bc65cfc852db22a2f18
Kapil_Kapil <148694211+Kapil-16082002@users.noreply.github.com>
2024-03-29 10:21:59 -0700

    Update Get equal substrings within budget.cpp

--- Searching2/Get equal substrings within budget.cpp
+++ Searching2/Get equal substrings within budget.cpp
@@ -1,1 +1,43 @@
-#
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool func(vector<int>&v, int c,int m){
+        int l=0;
+        int r=m;
+        int f=false;
+        int s;
+        while(r<v.size()){
+            s=v[r]-v[l];
+            if(c>=s){
+                f=true;
+                break;
+            }
+            l++;
+            r++;
+        }
+        return f;
+    }
+
+int equalSubstring(string s, string t, int maxCost) {
+        int i,n=s.size();
+        vector<int>prev(n+1,0);
+        for(i=1;i<=n;i++){
+            prev[i]=prev[i-1]+abs((s[i-1]-'a')-(t[i-1]-'a'));
+        }
+        int low=0;
+        int high=s.size()+1;
+        int res=0;
+        while(low<=high){
+            int m=(low+high)/2;
+            if(func(prev,maxCost,m)==true){
+                low=m+1;
+                res=m;
+            }
+            else{
+                high=m-1;
+            }
+        }
+        return res;
+    }

669454652c05f35ff2ebd75d9c65979d7d283fea
Kapil_Kapil <148694211+Kapil-16082002@users.noreply.github.com>
2024-05-05 23:11:20 +0530

    Update City_with_Smallest_No_of_Neighbours.cpp

---
+++ Graphs5/City_with_Smallest_No_of_Neighbours.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dist(n,vector<int>(n,INT_MAX));
+        for(auto it:edges){
+            dist[it[0]][it[1]]=it[2];
+            dist[it[1]][it[0]]=it[2];     
+        }
+        for(int i=0;i<n;i++){
+            dist[i][i]=0;
+        }
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dist[i][k]==INT_MAX || dist[k][j]==INT_MAX){
+                        continue;
+                    }
+                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+        int ccity=n;
+        int cityno=-1;
+        for(int city=0;city<n;city++){
+            int cnt=0;
+            for(int adjcity=0;adjcity<n;adjcity++){
+                if(dist[city][adjcity]<=distanceThreshold){
+                    cnt++;
+                }
+            }
+            if(cnt<=ccity){
+                ccity=cnt;
+                cityno=city;
+            }
+        }
+        return cityno;   
+    }
+};
